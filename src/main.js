c9078adf2a335055549ea59666e3da311c704fca
lmilanlsz <lmilanlsz@outlook.com>
2022-02-22 17:22:46 +0800

    first save

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import './plugins/element.js'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+    .use(ElementPlus)
+    .use(store)
+    .use(router)
+    .mount('#app')
